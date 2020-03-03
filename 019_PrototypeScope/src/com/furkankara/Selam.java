517ff29590d99ed1f94791d62d621c9924bef79e
Furkan9528 <Furkan9528@github.com>
2020-03-03 15:54:20 +0100

    Example of PrototypeScope

---
+++ 019_PrototypeScope/src/com/furkankara/Selam.java
@@ -0,0 +1,22 @@
+package com.furkankara;
+
+public class Selam {
+
+	private String mesaj;
+
+	public String getMesaj() {
+		return mesaj;
+	}
+
+	public void setMesaj(String mesaj) {
+		this.mesaj = mesaj;
+	}
+	
+	public void goster() {
+		
+		System.out.println(getMesaj());
+		
+	}
+
+	
+}
